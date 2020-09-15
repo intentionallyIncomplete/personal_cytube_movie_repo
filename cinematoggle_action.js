0c94ddb788271b2af3c4546e7c1cd872bbff0cf0
AlwaysRushing <alwaysinarush@protonmail.ch>
2020-09-15 02:34:16 -0700

    More literal syntax

--- cinematoggle_action.js
+++ cinematoggle_action.js
@@ -17,9 +17,9 @@
 // Store general options tab in a variable.
 // Create a select element and set ID attribute.
 $("#us-theme").hide();
-let selectorLocation = $("#us-general .form-horizontal .form-group .col-sm-8");
-let themeSelector = document.createElement("select");
-themeSelector.setAttribute("id","theme-list");
+// let selectorLocation = $("#us-general .form-horizontal .form-group .col-sm-8");
+// let themeSelector = document.createElement("select");
+// themeSelector.setAttribute("id","theme-list");
 
 // 2D array for storing values of theme options
 // that are for the custom theme selection dropdown.
@@ -32,8 +32,7 @@ let themesArray = [
 ];
 
 for (i in themesArray) {
-		themeSelector.append('<option value="'+themesArray[i][1]+'">'+themesArray[i][0]+'</option>');
-    selectorLocation.append(themeSelector);
+		$("#us-theme").parent().append('<option value="'+themesArray[i][1]+'">'+themesArray[i][0]+'</option>');
 	}
 
 "use strict";
