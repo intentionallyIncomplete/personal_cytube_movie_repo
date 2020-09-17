50089b3e50f5b4bb5e0c50dceb4a956c64b70106
noChar <alwaysinarush@protonmail.ch>
2020-09-17 14:40:24 -0700

    Did I really forget to add the theme changing function

--- cinematoggle_action.js
+++ cinematoggle_action.js
@@ -20,11 +20,36 @@ hween.value = "https://raw.githack.com/intentionallyIncomplete/personal_cytube_m
 
 $("#us-theme").append(hween);
 
-var theme = DEFAULT_THEME;
+(function () {
+  var c = document.cookie.split(";").map(function (s) {
+    return s.trim();
+  });
 
-if (theme == null || !theme.match(/^\/css\/themes\/.+?.css$/)) {
+  // Set in the head template.
+  var theme = DEFAULT_THEME;
+  for (var i = 0; i < c.length; i++) {
+    if (c[i].indexOf("cytube-theme=") === 0) {
+      theme = c[i].split("=")[1];
+      break;
+    }
+  }
+
+  if (theme == null || !theme.match(/^\/css\/themes\/.+?.css$/)) {
     return;
   }
+
+  if (theme !== DEFAULT_THEME) {
+    console.info("THEME COOKIE:", theme);
+    var cur = document.getElementById("usertheme");
+    cur.parentNode.removeChild(cur);
+    var css = document.createElement("link");
+    css.setAttribute("rel", "stylesheet");
+    css.setAttribute("type", "text/css");
+    css.setAttribute("href", theme);
+    css.setAttribute("id", "usertheme");
+    document.head.appendChild(css);
+  }
+})();
 
 /*____________________________________*/
 //  BEGIN CINEMA-MODE CONFIGURATION  //
