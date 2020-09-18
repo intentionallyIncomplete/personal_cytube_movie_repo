0a8b27bcfc0dbff63affc421896c4d4b85bd5e91
AlwaysRushing <alwaysinarush@protonmail.ch>
2020-09-18 02:02:31 -0700

    Going to revert if this doesn't work.

--- cinematoggle_action.js
+++ cinematoggle_action.js
@@ -25,42 +25,11 @@ hween.value = "https://raw.githack.com/intentionallyIncomplete/personal_cytube_m
   //  $("#userlist_afk").append(afkEye);
 
 $("#us-theme").append(hween);
-hween.onChange(function (){
+$("#us-theme").on("change", function(){
   if(!$("body").hasClass("hween")){
     $("body").toggleClass("hween");
   }
 });
-
-(function () {
-  var c = document.cookie.split(";").map(function (s) {
-    return s.trim();
-  });
-
-  // Set in the head template.
-  var theme = DEFAULT_THEME;
-  for (var i = 0; i < c.length; i++) {
-    if (c[i].indexOf("cytube-theme=") === 0) {
-      theme = c[i].split("=")[1];
-      break;
-    }
-  }
-
-  if (theme == null || !theme.match(/^\/css\/themes\/.+?.css$/)) {
-    return;
-  }
-
-  if (theme !== DEFAULT_THEME) {
-    console.info("THEME COOKIE:", theme);
-    var cur = document.getElementById("usertheme");
-    cur.parentNode.removeChild(cur);
-    var css = document.createElement("link");
-    css.setAttribute("rel", "stylesheet");
-    css.setAttribute("type", "text/css");
-    css.setAttribute("href", theme);
-    css.setAttribute("id", "usertheme");
-    document.head.appendChild(css);
-  }
-})();
 
 /*____________________________________*/
 //  BEGIN CINEMA-MODE CONFIGURATION  //
