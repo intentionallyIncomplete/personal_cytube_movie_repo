7281e428b77f90691d5904ec378c87cf505b168a
noChar <alwaysinarush@protonmail.ch>
2020-09-17 21:17:39 -0700

    Added "hween" class to body to see if that works for removing background

--- cinematoggle_action.js
+++ cinematoggle_action.js
@@ -53,6 +53,7 @@ $("#us-theme").append(hween);
 
 var curr_usertheme = document.getElementById("usertheme");
 if(curr_usertheme.href == hween.value){
+  $("body").toggleClass("hween");
   $("span.glyphicon.glyphicon-time").hide();
   let afk-eye = document.createElement("img");
   afk-eye.setAttribute("src","https://www.flaticon.com/svg/static/icons/svg/2213/2213665.svg");
