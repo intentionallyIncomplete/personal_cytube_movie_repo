eaba79efa921fe10bfe01872576061d6c0feaa09
Quigly <alwaysinarush@protonmail.com>
2022-03-30 08:20:27 -0700

    Update to Chat Columns

--- quiglys_customs.js
+++
@@ -1,6 +0,0 @@
-/*!
-**| Custom themes and additions to the main Cytu.be website by Quigly.
-**| Free to use at your own discretion but just credit me please.
-**| Version 2020.09.14
-**| @preserve
-*/
